9b0be4b3d05379d468181bade840d7d3e0796ad2
derLesh <derLesh@users.noreply.github.com>
2023-11-10 16:02:46 +0000

    deploy: 94408919562a9b64624e08aa7f0159d0b8aa6e81

---
+++ Tag 1/lösung.md
@@ -0,0 +1,27 @@
+# Lösung - Tag 1
+
+[https://github.com/derLesh/JKSchatzsuche/raw/main/Tag%201/JKSchatzsuche-Tag1.mp4](https://github.com/derLesh/JKSchatzsuche/assets/11671272/d8f6bc38-0b8f-4e31-bfc6-888fa4393cc2)
+
+An Tag 1 **Das Frühstück** wurde mehrfach *So* und *Wie* gesagt.
+
+Entscheidend ist hier die Anzahl und das Gebäck von Joko und Klaas.
+
+- Joko erhält ein rundes Gebäck mit einem Loch in der Mitte = 0
+- Klass erhält ein längliches Gebäck = 1
+
+Genannt wurde folgendes:
+
+`So Wie So So So So So Wie So Wie Wie Wie So So So So So Wie Wie So So So So Wie So Wie Wie So So So Wie Wie So Wie Wie So Wie So So So So Wie Wie So So Wie So Wie `
+
+Übersetzt kommt heraus:
+
+`0 1 0 0 0 0 0 1 0 1 1 1 0 0 0 0 0 1 1 0 0 0 0 1 0 1 1 0 0 0 1 1 0 1 1 0 1 0 0 0 0 1 1 0 0 1 0 1`
+
+Diesen Binärcode kann man [übersetzten](https://gc.de/gc/binaer/) lassen und ergibt:
+
+`Apache`
+
+Mit dem Lösungwort ist der Rapper **Apache 207** gemeint.
+
+Die Lösungziffern lauten: **2 0 7**
+
---
+++ coordinates.py
@@ -0,0 +1,75 @@
+# %%
+# Installieren der benötigten Bibliotheken für folium
+# !pip install folium branca
+
+# %%
+# Importieren der notwendigen Bibliotheken
+import itertools
+import folium
+import branca
+import geopandas as gpd
+from shapely.geometry import MultiPoint
+import os
+# from shapely.geometry import Point
+# from IPython.display import display
+
+# %%
+# Funktion zur Erzeugung von Google Maps URLs und zur Kartenvisualisierung mit folium
+def generate_google_maps_urls_and_folium_map(breitengrad_pattern, laengengrad_pattern, breiten_range, laengen_range):
+    urls = []
+    points = []
+
+    # Ersetze Unterstriche mit Platzhalter für Ziffern
+    breitengrad_pattern = breitengrad_pattern.replace("_", "{}")
+    laengengrad_pattern = laengengrad_pattern.replace("_", "{}")
+    
+    # Generiere alle möglichen Zahlen für die Platzhalter
+    breiten_digits = itertools.product(range(10), repeat=breitengrad_pattern.count("{}"))
+    laengen_digits = list(itertools.product(range(10), repeat=laengengrad_pattern.count("{}")))
+    
+    # Erstelle alle möglichen Koordinaten innerhalb des vorgegebenen Rahmens
+    for breiten_digit in breiten_digits:
+        for laengen_digit in laengen_digits:
+            breitengrad = float(breitengrad_pattern.format(*breiten_digit))
+            laengengrad = float(laengengrad_pattern.format(*laengen_digit))
+            if breiten_range[0] <= breitengrad <= breiten_range[1] and laengen_range[0] <= laengengrad <= laengen_range[1]:
+                urls.append(f"https://www.google.com/maps?q={breitengrad},{laengengrad}")
+                points.append((breitengrad, laengengrad))
+
+    # Erstelle ein MultiPoint-Objekt aus den Punkten
+    multi_point = MultiPoint(points)
+    # Erstelle ein GeoSeries-Objekt und berechne den Centroid
+    centroid = gpd.GeoSeries([multi_point]).centroid.iloc[0]
+    
+    # Erstellen der Folium-Karte zentriert auf den Centroid
+    m = folium.Map(location=[centroid.y, centroid.x], zoom_start=6)
+
+    # Füge den Centroid als Marker zur Karte hinzu
+    folium.Marker([centroid.y, centroid.x]).add_to(m)
+
+    # Karte anzeigen
+    m.save("coordinates.html")
+
+    return urls
+
+# %%
+# Eingabeaufforderung für den Benutzer
+breitengrad_pattern = os.getenv('BREITENGRAD_PATTERN')
+if not breitengrad_pattern:
+    breitengrad_pattern = input("Bitte geben Sie das Muster für den Breitengrad ein (z.B. '50._7_6_'): ")
+
+laengengrad_pattern = os.getenv('LAENGENGRAD_PATTERN')
+if not laengengrad_pattern:
+    laengengrad_pattern = input("Bitte geben Sie das Muster für den Längengrad ein (z.B. '12.4_2__'): ")
+
+# %%
+# Definieren des Bereichs für Koordinaten in Deutschland
+breiten_range = (47.00000, 55.99999)
+laengen_range = (6.00000, 15.99999)
+
+# %%
+# Generierung und Ausgabe der URLs und Anzeigen der Karte
+urls = generate_google_maps_urls_and_folium_map(breitengrad_pattern, laengengrad_pattern, breiten_range, laengen_range)
+print(f"\nEs wurden {len(urls)} URLs generiert:\n")
+for url in urls:
+    print(url)
